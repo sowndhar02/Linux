8687d08f391c931ba8d92e21c8368c73b9d02b7e
sowndhar02 <153707732+sowndhar02@users.noreply.github.com>
2024-07-09 14:00:07 +0530

    Add files via upload

---
+++ pwd.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<limits.h>
+int main(int argc,char *argv[])
+{
+	char cwd[PATH_MAX];
+
+	if(getcwd(cwd,sizeof(cwd))!= NULL)
+	{
+		printf("%s\n",cwd);
+
+	}
+	else
+	{
+		perror("getcwd() error");
+		return 1;
+	}
+
+
+}
+
